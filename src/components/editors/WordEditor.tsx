cafb73cab6bcb5a010924a32f395ffacd5b499ce
aetherom <299110591+aetherom@users.noreply.github.com>
2026-08-16 22:34:21 +0100

    Create WordEditor.tsx

---
+++ src/components/editors/WordEditor.tsx
@@ -0,0 +1,7 @@
+export default function EditorPlaceholder() {
+  return (
+    <div className="w-full h-full flex items-center justify-center bg-slate-50 text-slate-400">
+      <p>Advanced WASM Editor Engine Loads Here...</p>
+    </div>
+  );
+}
