62c45e2641ea19fdbb1b5d53d3f332b8d61052a2
aetherom <299110591+aetherom@users.noreply.github.com>
2026-08-16 23:09:22 +0100

    Create ExcelEditor.tsx

---
+++ src/components/editors/ExcelEditor.tsx
@@ -0,0 +1,7 @@
+export default function ExcelEditor() {
+  return (
+    <div className="w-full h-full flex items-center justify-center bg-slate-50 text-slate-400">
+      <p>Advanced WASM Excel Editor Loads Here...</p>
+    </div>
+  );
+}
