5fc83a2878eb5adcad4233468d4c21cad93362b8
aetherom <299110591+aetherom@users.noreply.github.com>
2026-08-16 23:09:50 +0100

    Create ImageEditor.tsx

---
+++ src/components/editors/ImageEditor.tsx
@@ -0,0 +1,7 @@
+export default function ImageEditor() {
+  return (
+    <div className="w-full h-full flex items-center justify-center bg-slate-50 text-slate-400">
+      <p>Advanced WebGL Image Editor Loads Here...</p>
+    </div>
+  );
+}
