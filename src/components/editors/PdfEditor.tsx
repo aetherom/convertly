e5d4cda96bbd1fc8c4173cd377b2d6458ea952c9
aetherom <299110591+aetherom@users.noreply.github.com>
2026-08-16 23:09:38 +0100

    Create PdfEditor.tsx

---
+++ src/components/editors/PdfEditor.tsx
@@ -0,0 +1,7 @@
+export default function PdfEditor() {
+  return (
+    <div className="w-full h-full flex items-center justify-center bg-slate-50 text-slate-400">
+      <p>Advanced WASM PDF Editor Loads Here...</p>
+    </div>
+  );
+}
